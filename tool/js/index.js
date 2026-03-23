3802b28fa39c5d57d60d2981831e68650485ef8d
cod-git <yuuto.1212.20@gmail.com>
2026-03-24 07:30:18 +0900

    Added index.js and index.css

---
+++ tool/js/index.js
@@ -0,0 +1,23 @@
+function converter() {
+    window.location.assign("tool/unit_converter.html");
+}
+
+function counter() {
+    window.location.assign("tool/character_counter.html");
+}
+
+function endecoder() {
+    window.location.assign("tool/encod_decod.html");
+}
+
+function password() {
+    window.location.assign("tool/password_checker.html");
+}
+
+function qrcode() {
+    window.location.assign("tool/qrcode_reader.html");
+}
+
+function tournament() {
+    window.location.assign("tool/Tournament.html");
+}
